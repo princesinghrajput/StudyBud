9e3e656375a2509112a1c26a931961def3896ebe
princesinghrajput <pkumar@sensation.com>
2024-06-21 18:25:54 +0530

    setup views & urls

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def room(request):
+    return HttpResponse("Hello, world.Room Index.")
+
