59d4cc4d1c4f4fc6d97f5703fe35f5d8a425cc78
princesinghrajput <psr8084@gmail.com>
2024-07-15 11:01:44 +0530

    Enhanced search and setup login func

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -14,14 +21,44 @@ from django.http import HttpResponse
     
 # ]
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User Does Not Exist')
+
+        user= authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        
+        else:
+            messages.error(request, 'Username OR password does not exist')
+
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 def home(request):
+
     q=request.GET.get('q') if request.GET.get('q') !=None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__contains=q) | 
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
 
     topics = Topic.objects.all()
     
+    room_count = rooms.count()
+    
 
-    context = {'rooms':rooms, 'topics':topics}
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
