54ffd9a0ed38f2c8f8dd1f8ca9be7339aeb6be4a
princesinghrajput <psr8084@gmail.com>
2024-07-18 10:37:06 +0530

    create msg done

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -9,7 +9,7 @@ from django.contrib import messages
 
 
 
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -98,6 +98,15 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created_at')
+    
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+    
     context = {'room':room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
